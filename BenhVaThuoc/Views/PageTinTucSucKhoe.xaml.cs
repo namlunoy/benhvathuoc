c142375e9d6fe5e6ec68af1cb7df13607e77e892
vuanhtam316 <vuanhtam316@gmail.com>
2015-12-01 21:24:38 +0700

    xem ho t cai, tai sao lai khong load dc string len

--- BenhVaThuoc/Views/PageTinTucSucKhoe.xaml.cs
+++ BenhVaThuoc/Views/PageTinTucSucKhoe.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using System.Diagnostics;
+using System.IO;
 
 namespace BenhVaThuoc.Views
 {
@@ -16,19 +17,56 @@ namespace BenhVaThuoc.Views
         public PageTinTucSucKhoe()
         {
             InitializeComponent();
-            Loaded += TinTucSucKhoe_Loaded;
+            //Loaded += TinTucSucKhoe_Loaded;
+            UseWebClient("http://vnexpress.net/rss/suc-khoe.rss");
         }
 
-        void TinTucSucKhoe_Loaded(object sender, RoutedEventArgs e)
+        private void UseWebClient(string address)
         {
-            Debug.WriteLine("TinTucSucKhoe_Loaded");
+            WebClient client = new WebClient();
+            Uri uri = new Uri(address);
+            client.DownloadStringCompleted += new DownloadStringCompletedEventHandler(DownloadStringCallback);
+            client.DownloadStringAsync(uri);
         }
 
-        private void Click_XemChiTiet(object sender, RoutedEventArgs e)
+        private void DownloadStringCallback(object sender, DownloadStringCompletedEventArgs e)
         {
-            MainPage.Current.ShowChildViewNext(this, new PageChiTietBaiBao());
+            //string text = e.Result;
+            System.Diagnostics.Debugger.Break();
         }
 
+
+        //void TinTucSucKhoe_Loaded(object sender, RoutedEventArgs e)
+        //{
+        //    Debug.WriteLine("TinTucSucKhoe_Loaded");
+        //}
+
+        //private void Click_XemChiTiet(object sender, RoutedEventArgs e)
+        //{
+        //    MainPage.Current.ShowChildViewNext(this, new PageChiTietBaiBao());
+        //}
+        //private void tile_Tap()
+        //{
+        //    WebClient wc = new WebClient();
+        //    wc.OpenReadCompleted += new OpenReadCompletedEventHandler(wc_OpenReadCompleted);
+
+        //    wc.OpenReadAsync(new Uri("http://vnexpress.net/rss/suc-khoe.rss"));
+        //}
+
+        //void wc_OpenReadCompleted(object sender, OpenReadCompletedEventArgs e)
+        //{
+        //    try
+        //    {
+        //        StreamReader s = new StreamReader(e.Result);
+        //        string r = s.ReadToEnd();
+        //        MessageBox.Show(r);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show(ex.Message);
+        //    }
+        //}
+        
        
     }
 }
