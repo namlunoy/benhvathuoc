5d8df03bdfa2388fbc1b6a1a4f8d36d83f5e04f5
thebrownbox <hoangvancong123@gmail.com>
2015-12-20 15:39:31 +0700

    Update map

--- BenhVaThuoc/Views/PageTimKiem.xaml.cs
+++ BenhVaThuoc/Views/PageTimKiem.xaml.cs
@@ -14,13 +14,14 @@ using System.Windows.Media;
 using Microsoft.Phone.Maps.Controls;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace BenhVaThuoc.Views
 {
     public partial class PageTimKiem : UserControl
     {
         string s = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=nha+thuoc&key=AIzaSyDjpYbcGOvN7GxL7OnovQQz1dBxhFUrBzE&location={0},{1}&radius=100";
-        
+
         public PageTimKiem()
         {
             InitializeComponent();
@@ -33,7 +34,7 @@ namespace BenhVaThuoc.Views
         }
 
 
-
+        MapLayer myLocationLayer;
 
         private async void ShowMyLocationOnTheMap()
         {
@@ -46,30 +47,16 @@ namespace BenhVaThuoc.Views
             this.map.Center = myGeoCoordinate;
             this.map.ZoomLevel = 13;
 
-          
-
-            // Create a small circle to mark the current location.
-            Ellipse myCircle = new Ellipse();
-            myCircle.Fill = new SolidColorBrush(Colors.Blue);
-            myCircle.Height = 20;
-            myCircle.Width = 20;
-            myCircle.Opacity = 50;
 
 
-            // Create a MapOverlay to contain the circle.
-            MapOverlay myLocationOverlay = new MapOverlay();
-            myLocationOverlay.Content = myCircle;
-            myLocationOverlay.PositionOrigin = new Point(0.5, 0.5);
-            myLocationOverlay.GeoCoordinate = myGeoCoordinate;
+      
 
 
             // Create a MapLayer to contain the MapOverlay.
-            MapLayer myLocationLayer = new MapLayer();
-            myLocationLayer.Add(myLocationOverlay);
-
-
+             myLocationLayer = new MapLayer();
             map.Layers.Add(myLocationLayer);
 
+            Draw(myGeoCoordinate);
 
             await HienThiCacNhaThuoc(myGeoCoordinate);
         }
@@ -85,10 +72,62 @@ namespace BenhVaThuoc.Views
             Debug.WriteLine("url : " + url);
         }
 
+
+        void Draw(GeoCoordinate myGeoCoordinate)
+        {
+            // Create a small circle to mark the current location.
+            Ellipse myCircle = new Ellipse();
+            myCircle.Fill = new SolidColorBrush(Colors.Blue);
+            myCircle.Height = 20;
+            myCircle.Width = 20;
+            myCircle.Opacity = 50;
+
+
+            // Create a MapOverlay to contain the circle.
+            MapOverlay myLocationOverlay = new MapOverlay();
+            myLocationOverlay.Content = myCircle;
+            myLocationOverlay.PositionOrigin = new Point(0.5, 0.5);
+            myLocationOverlay.GeoCoordinate = myGeoCoordinate;
+
+            myLocationLayer.Add(myLocationOverlay);
+        }
+
+        void Draw(Location location)
+        {
+            // Create a small circle to mark the current location.
+            Ellipse myCircle = new Ellipse();
+            myCircle.Fill = new SolidColorBrush(Colors.Red);
+            myCircle.Height = 20;
+            myCircle.Width = 20;
+            myCircle.Opacity = 50;
+
+
+            // Create a MapOverlay to contain the circle.
+            MapOverlay myLocationOverlay = new MapOverlay();
+            myLocationOverlay.Content = myCircle;
+            myLocationOverlay.PositionOrigin = new Point(0.5, 0.5);
+            myLocationOverlay.GeoCoordinate = new GeoCoordinate(location.lat,location.lng);
+
+            myLocationLayer.Add(myLocationOverlay);
+        }
+
         void clien_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
         {
-            
+            if (e.Error == null)
+            {
+
+                Debug.WriteLine("Resouce: "+e.Result);
+
+                RootObject root = JsonConvert.DeserializeObject<RootObject>(e.Result);
+                foreach (var item in root.results)
+                {
+                    Debug.WriteLine(item.geometry.location.lat);
+                    Draw(item.geometry.location);
+                }
+            }
         }
+
+
 
     }
 }
